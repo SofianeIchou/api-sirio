ed307c18fb3dace882c59b6e54f9f4ad2a85f103
SofianeIchou <sofiane.ichou.jrbl@gmail.com>
2022-05-30 07:41:30 +0200

    first commit

---
+++ src/main/java/com/example/apimaroma/FirebaseInitialize.java
@@ -0,0 +1,38 @@
+package com.example.apimaroma;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
+
+@Service
+public class FirebaseInitialize {
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigPath;
+
+    @PostConstruct
+    public void initialize(){
+        try {
+            System.out.println("-----------------------path");
+
+            InputStream serviceAccount = new ClassPathResource(firebaseConfigPath).getInputStream();
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://projet-f193c/firestore/firebaseio.com/")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        }catch (Exception e){
+            System.out.println("-----------------------err");
+            e.printStackTrace();
+        }
+
+    };
+}
---
+++ src/main/java/com/example/apimaroma/categories/CategoryService.java
@@ -0,0 +1,40 @@
+package com.example.apimaroma.categories;
+
+import com.example.apimaroma.products.ProductBean;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class CategoryService {
+    private Firestore dbFirestore = FirestoreClient.getFirestore();
+    private CollectionReference categoriesTable = dbFirestore.collection("categories");
+
+    public List<CategoryBean> getAllCategories() throws ExecutionException, InterruptedException{
+        ApiFuture<QuerySnapshot> future =
+                categoriesTable.get();
+
+        // block on response
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        List<CategoryBean> categoryBeans = new ArrayList<>();
+
+        for (DocumentSnapshot document : documents) {
+            System.out.println(document.getId() + " => " + document.toObject(CategoryBean.class));
+            categoryBeans.add(document.toObject(CategoryBean.class));
+        }
+        System.out.println(categoryBeans);
+        return categoryBeans;
+    }
+
+    public CategoryBean getCategory(String id) throws ExecutionException, InterruptedException {
+        DocumentReference documentReference = categoriesTable.document(id);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+        DocumentSnapshot document = future.get();
+        return  document.toObject(CategoryBean.class);
+    }
+}
